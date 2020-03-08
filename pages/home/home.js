4467f9b3e4d238951acc2364e5db9b6baf34dfd6
865713897@qq.com <865713897@qq.com>
2020-03-08 23:16:35 +0800

    知识点1

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,32 @@
 // pages/home/home.js
 Page({
-  
+  data: {
+    name: 'ilovecoding',
+    age: 18,
+    students: [
+      { id: 100, name: 'kobe', age: 30 },
+      { id: 101, name: 'james', age: 28 },
+      { id: 102, name: 'curry', age: 32 },
+      { id: 103, name: 'ilovecoding', age: 21 }
+    ],
+    counter: 0
+  },
+  // 点击按钮：counter+1
+  increment() {
+    // 1.错误的做法
+    // this.data.counter++;
+    // 2.正确的做法
+    this.setData({
+      counter: this.data.counter + 1
+    })
+  },
+  // 点击按钮：counter-1
+  decrement() {
+    // 1.错误的做法
+    // this.data.counter--;
+    // 2.正确的做法
+    this.setData({
+      counter: this.data.counter - 1
+    })
+  }
 })
